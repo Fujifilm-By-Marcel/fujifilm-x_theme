1aa61ae3817138dee2519a914d29cfc0f2fb4001
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2020-09-03 17:36:23 -0400

    centered modal windows vertically.

--- creators/archive-creators.php
+++ creators/archive-creators.php
@@ -1,7 +1,7 @@
 <?php 
 function load_usa_js_css(){
 	wp_enqueue_style('materialize', get_stylesheet_directory_uri().'/en-us/css/materialize-gridonly.css', false, NULL, 'all');
-	wp_enqueue_style('archive-creators', get_stylesheet_directory_uri().'/en-us/creators/css/archive-creators.css', array(),'1.1.31');
+	wp_enqueue_style('archive-creators', get_stylesheet_directory_uri().'/en-us/creators/css/archive-creators.css', array(),'1.1.32');
 	wp_enqueue_script('uscommon', get_stylesheet_directory_uri().'/en-us/js/common.js', array(), '1.0.0', true);
 } 
 add_action( 'wp_enqueue_scripts', 'load_usa_js_css' );
