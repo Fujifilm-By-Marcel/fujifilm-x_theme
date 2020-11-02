dc0491837d106146f9ed03d438fcac1a25020253
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2020-11-02 09:32:09 -0500

    moved quick start guides to subdir

---
+++ fnac-assets/quick-start-guide/japan/archive-quick-start-guide.php
@@ -0,0 +1,3 @@
+<?php 
+require get_stylesheet_directory().'/en-us/fnac-assets/quick-start-guide/archive-quick-start-guide.php';
+?>
---
+++ fnac-assets/quick-start-guide/japan/single-quick-start-guide.php
@@ -0,0 +1,3 @@
+<?php 
+require get_stylesheet_directory().'/en-us/fnac-assets/quick-start-guide/single-quick-start-guide.php';
+?>
