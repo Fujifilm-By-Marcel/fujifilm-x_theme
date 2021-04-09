76bde756ec3ffb208ec09010a6a085cf085937ba
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-04-09 10:22:48 -0400

    added terms modal

--- page-wps.php
+++ page-wps.php
@@ -346,6 +346,44 @@ section{
 	margin:auto
 }
 .main p>a{text-decoration: underline;}
+
+/* The Modal (background) */
+.custom-modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.modal-close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.modal-close:hover,
+.modal-close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
 </style>
 <section class="main">
 
@@ -463,8 +501,43 @@ section{
 	<?php } 
 	endif; ?>
 
+
+	<div id="myModal" class="custom-modal">
+	  <div class="modal-content" style="max-width:70em;">
+	    <span class="modal-close">&times;</span>
+	    <p><?php the_field("promo_terms"); ?></p>
+	  </div>
+	</div>
+
 </section>
 <script>	
+
+	// Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("termsbtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("modal-close")[0];
+
+	// When the user clicks on the button, open the modal
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+
 	jQuery(function($) {
 		
 		/*
