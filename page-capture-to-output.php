622efad3fb99607ae9a4ff3ae26cf5d659deb649
marcel.munevar.contractor <marcel.munevar.contractor@fujifilm.com>
2021-08-04 09:35:50 -0400

    modals

--- page-capture-to-output.php
+++ page-capture-to-output.php
@@ -142,7 +142,7 @@ section.cto-navigation{
 	width: min(90%, 16ch);
 	font-family: var(--accent-font);
 	margin:auto;
-	margin-bottom:2.375rem;
+	
 
 }
 
@@ -170,10 +170,12 @@ section.cto-navigation{
 @media (min-width:50em) {
 	.split{
 		flex-direction:row;
-	}
-	
+	}	
 	.split > *{
 		flex-basis: 100%;
+	}
+	.split.no-basis > *{
+		flex-basis: auto;	
 	}
 	.split > * + * {
 		margin-left:2em;
@@ -312,14 +314,6 @@ section.cto-navigation{
 }
 
 
-
-.margin-bottom-1{
-	margin-bottom:1rem !important;
-}
-.margin-bottom-2{
-	margin-bottom:2rem !important;
-}
-
 .tech-1, .tech-2, .tech-3{
 	display: none;
 	height: 25em;
@@ -397,6 +391,136 @@ section.cto-navigation{
     width: 100%; 
     z-index: 11;
 }
+
+/* modal */
+.mymodal{
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0,0,0,.5);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: 40;
+	flex-flow:column nowrap;
+}
+.mymodal-inner{	
+	max-height: 100%;
+	overflow-y: auto;
+	position: relative;
+}
+
+.close-modal{
+	position: absolute;
+	top: 0;
+	right: 0;
+	cursor: pointer;
+	margin: 10px 15px;
+	font-size: 1.25rem;
+}
+
+.mymodal.terms .mymodal-inner{
+	background: white;
+	font-size: 1rem;
+	padding: 3.625rem 2.25rem 3.625rem;
+	max-width: 50em;
+}
+
+.form-modals .mymodal-inner{
+	background: black;
+	color: white;
+	font-size: 1rem;
+	padding: 5.625rem 2.25rem 3.625rem;
+	max-width: 405px;
+}
+
+.form-modals .previous-modal-button{
+	font-size: 0.625rem;
+}
+
+/* helper classes */
+.margin-bottom-1{
+	margin-bottom:1rem !important;
+}
+.margin-bottom-2{
+	margin-bottom:2rem !important;
+}
+.float-right{
+	float: right;
+}
+.fjalla{
+	font-family: var(--accent-font);
+}
+
+
+/* radio buttons */
+
+/* The container */
+.radio-input {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default radio button */
+.radio-input input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom radio button */
+.checkmark {
+  position: absolute;
+  top: 2px;
+  left: 0;
+  height: 20px;
+  width: 20px;
+  border: 1px solid white;
+  border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+/*.radio-input:hover input ~ .checkmark {
+  background-color: #ccc;
+}*/
+
+/* When the radio button is checked, add a blue background */
+/*.radio-input input:checked ~ .checkmark {
+  background-color: #2196F3;
+}*/
+
+/* Create the indicator (the dot/circle - hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the indicator (dot/circle) when checked */
+.radio-input input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the indicator (dot/circle) */
+.radio-input .checkmark:after {
+	width: 14px;
+	height: 14px;
+	border-radius: 50%;
+	background: white;
+	left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
 </style>
 <section class="main page-cto">
 	<?php if( get_field('tagline_section')['button_text'] ): ?>
@@ -405,7 +529,7 @@ section.cto-navigation{
 			<!-- todo: insert logo -->
 			<img style="width:81px;margin:.9rem;display:inline;" src="<?php the_field('logo'); ?>">
 			<ul class="cto-menu">
-				<li class="cto-menu-item active"><a href="<?php echo get_field('tagline_section')['button_link'] ?>" target="<?php echo get_field('tagline_section')['button_target'] ?>"><?php echo get_field('tagline_section')['button_text'] ?></a></li>
+				<li class="cto-menu-item active"><a href="<?php echo get_field('tagline_section')['button_link'] ?>" target="<?php echo get_field('tagline_section')['button_target'] ?>" class="<?php echo get_field('tagline_section')['button_class'] ?>"><?php echo get_field('tagline_section')['button_text'] ?></a></li>
 			</ul>
 		</div>
 	</section>
@@ -416,8 +540,9 @@ section.cto-navigation{
 			<?php if( get_field('tagline_section')['tagline'] ): ?>
 			<div class="center-vertically">
 				<div class="center-text vertical-padding-3">
-					<p class="page-tagline"><?php echo get_field('tagline_section')['tagline'] ?></p>
-					<a href="<?php echo get_field('tagline_section')['button_link'] ?>" class="button" target="<?php echo get_field('tagline_section')['button_target'] ?>"><?php echo get_field('tagline_section')['button_text'] ?></a>
+					<p class="page-tagline margin-bottom-1"><?php echo get_field('tagline_section')['tagline'] ?></p>
+					<?php if( get_field('tagline_section')['terms_button_text'] != ""  ) { ?> <div class="margin-bottom-1" style="width: min(90%, 19ch);margin: auto;"><?php echo get_field('tagline_section')['terms_button_text'] ?></div><?php } ?>
+					<a href="<?php echo get_field('tagline_section')['button_link'] ?>" class="button <?php echo get_field('tagline_section')['button_class'] ?>" target="<?php echo get_field('tagline_section')['button_target'] ?>"><?php echo get_field('tagline_section')['button_text'] ?></a>
 				</div>
 			</div>
 			<?php endif; ?>
@@ -491,7 +616,7 @@ section.cto-navigation{
 			background: linear-gradient(to bottom, black 0%,black <?php echo $photo_paper['background_gradient_percentage_mobile'] ?>,#000000 <?php echo $photo_paper['background_gradient_percentage_mobile'] ?>,white <?php echo $photo_paper['background_gradient_percentage_mobile'] ?>,white 100%); /* W3C */
 		}
 		@media (min-width:50em) {
-			.split{
+			/*.split{
 				flex-direction:row;
 			}
 			
@@ -500,7 +625,7 @@ section.cto-navigation{
 			}
 			.split > * + * {
 				margin-left:2em;
-			}
+			}*/
 
 			.black-white-bg{
 				background: linear-gradient(to bottom, black 0%,black <?php echo $photo_paper['background_gradient_percentage_desktop'] ?>,#000000 <?php echo $photo_paper['background_gradient_percentage_desktop'] ?>,white <?php echo $photo_paper['background_gradient_percentage_desktop'] ?>,white 100%); /* W3C */
@@ -551,7 +676,7 @@ section.cto-navigation{
 					</div>
 				</div>
 			</div>
-			<a href="<?php echo $photo_paper['button_link'] ?>" target="<?php echo $photo_paper['button_target'] ?>" class="button margin-bottom-1"><?php echo $photo_paper['button_text'] ?></a>
+			<a href="<?php echo $photo_paper['button_link'] ?>" target="<?php echo $photo_paper['button_target'] ?>" class="button margin-bottom-1 <?php echo $photo_paper['button_class'] ?>"><?php echo $photo_paper['button_text'] ?></a>
 			<p class="button-notes" style="font-size:.75rem;"><?php echo $photo_paper['button_notes'] ?></p>			
 		</div>
 	</section>
@@ -606,9 +731,130 @@ section.cto-navigation{
 		</div>
 	</section>
 	<?php endif; ?>
+
+
+	<?php
+	$form_modal = get_field('form_modal');
+	if( $form_modal ): ?>
+	<div class="form-modals">
+		<div class="mymodal" data-page="1" style="display:none;">
+			<div class="mymodal-inner">
+				<div class="close-modal">
+					<i class="fas fa-times"></i>
+				</div>
+				<!-- image, header, text, back, next -->
+				<h3><?php echo $form_modal['header']; ?></h3>
+				<?php echo  $form_modal['text']; ?>
+				<a class="float-right next-modal-button" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="63" height="29" fill="none" viewBox="0 0 63 29"><path fill="#fff" d="M60.06 16.06c.586-.585.586-1.535 0-2.12l-9.545-9.547c-.586-.585-1.536-.585-2.122 0-.585.586-.585 1.536 0 2.122L56.88 15l-8.486 8.485c-.585.586-.585 1.536 0 2.122.586.585 1.536.585 2.122 0l9.546-9.546zM0 16.5h59v-3H0v3z"/></svg></a>
+			</div>
+		</div>
+
+		<div class="mymodal" data-page="2" style="display:none;">
+			<div class="mymodal-inner">
+				<div class="close-modal">
+					<i class="fas fa-times"></i>
+				</div>
+				<!-- header, text, options (radio) -->
+				<p class="fjalla" style="margin-bottom:.125rem"><?php echo $form_modal['question']; ?></p>
+				<p class="margin-bottom-1" style="font-size:.75rem;"><?php echo $form_modal['question_text']; ?></p>
+
+				<?php 
+				$i = 0;
+				$rows = $form_modal['options'];;
+				if( $rows ) {
+				    $len = count($rows);				    
+				    foreach( $rows as $row ) {
+				    	$i++;
+				        $image = $row['image'];
+				        $class="margin-bottom-1";
+				        if ($i == $len) { $class=""; }
+				        echo '<label style="font-size:.875rem;" class="radio-input '.$class.'">'.$row['title'];
+				        echo '<input type="radio" name="selection" value="'.$i.'">';
+				        echo '<span class="checkmark"></span>';				        
+				        echo '</label>';
+				    }
+				}	?>	
+				<a class="float-right next-modal-button toggle-view" href="#" style="display:none;"><svg xmlns="http://www.w3.org/2000/svg" width="63" height="29" fill="none" viewBox="0 0 63 29"><path fill="#fff" d="M60.06 16.06c.586-.585.586-1.535 0-2.12l-9.545-9.547c-.586-.585-1.536-.585-2.122 0-.585.586-.585 1.536 0 2.122L56.88 15l-8.486 8.485c-.585.586-.585 1.536 0 2.122.586.585 1.536.585 2.122 0l9.546-9.546zM0 16.5h59v-3H0v3z"/></svg></a>
+			</div>
+		</div>
+
+
+		<!-- loop options -->
+		<?php 
+		$option = 0;
+		$rows = $form_modal['options'];;
+		if( $rows ) {
+	    $len = count($rows);
+	    foreach( $rows as $row ) {
+		    $option++; ?>
+				<div class="mymodal" data-page="3" data-option="<?php echo $option; ?>" style="display:none;">
+					<div class="mymodal-inner">
+						<div class="close-modal">
+							<i class="fas fa-times"></i>
+						</div>
+						<!-- image, header, text, back, next -->
+						<div class="split no-basis margin-bottom-1">
+							<div>
+								<img src="<?php echo $row['image']['url']; ?>" alt="" width="<?php echo $row['image']['width']; ?>" height="<?php echo $row['image']['height']; ?>" style="min-width:<?php echo $row['image']['width']; ?>px" class="margin-bottom-1">
+							</div>
+							<div>
+								<p class="fjalla" style="margin-bottom:.125rem"><?php echo $row['title']; ?></p>
+								<p style="font-size:.75rem;"><?php echo $row['text']; ?></p>
+							</div>									
+						</div>
+						<a href="#" class="previous-modal-button">GO BACK</a>
+						<a class="float-right next-modal-button" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="63" height="29" fill="none" viewBox="0 0 63 29"><path fill="#fff" d="M60.06 16.06c.586-.585.586-1.535 0-2.12l-9.545-9.547c-.586-.585-1.536-.585-2.122 0-.585.586-.585 1.536 0 2.122L56.88 15l-8.486 8.485c-.585.586-.585 1.536 0 2.122.586.585 1.536.585 2.122 0l9.546-9.546zM0 16.5h59v-3H0v3z"/></svg></a>
+					</div>
+				</div>
+				<?php
+			}
+		} ?>
+
+		<div class="mymodal" data-page="4" style="display:none;">
+			<div class="mymodal-inner">
+				<div class="close-modal">
+					<i class="fas fa-times"></i>
+				</div>
+				<!-- form -->
+				<iframe id="adestra-form" src="<?php echo $form_modal['modal_src']; ?>" ></iframe>
+			</div>
+		</div>
+	</div>
+	<?php endif; ?>
+
+
+	<div class="mymodal terms" style="display:none;">
+		<div class="mymodal-inner">
+				<div class="close-modal">
+					<i class="fas fa-times"></i>
+				</div>			
+				<p><?php the_field("terms"); ?></p>
+			</div>
+		</div>
+	</div>
+
 </section>
 
+
+
 <script>	
+
+	//recieve data from iframe
+	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+	eventer(messageEvent,function(e) {
+	    var key = e.message ? "message" : "data";
+	    var data = e[key];
+	    jQuery("#adestra-form").height(data);
+	},false);
+
+
+	//send data to iframe
+	function sendDataToIframe(data){
+		document.getElementById('adestra-form').contentWindow.postMessage(data ,"*");
+	}
+
 	jQuery(function($) {
 		
 
@@ -666,11 +912,9 @@ section.cto-navigation{
 		    nav:true,
 		    navText : ["<i class='fas fa-caret-left'></i>","<i class='fas fa-caret-right'></i>"],
 		    responsive : {
-			    // breakpoint from 0 up
 			    0 : {
 			        items:3,
 			    },
-			    // breakpoint from 480 up
 			    400 : {
 			        items:4,
 			    },	
@@ -680,6 +924,56 @@ section.cto-navigation{
 			    
 			}
 		});
+
+		$('.open-terms').click(function(){
+			$('.mymodal.terms').show();
+			return false;
+		});
+
+
+		$('.open-form').click(function(){
+
+			$('.form-modals .mymodal[data-page="1"]').show();
+		});
+
+		$('.mymodal').click(function( event ){
+			if( $(event.target).is(".mymodal") || $(event.target).closest( '.close-modal' ).length ){
+				$(event.target).closest('.mymodal').hide();
+			}			
+		});
+		
+		$(".form-modals .next-modal-button").click(function(event){
+			var page = $(this).closest('.mymodal').data("page");
+			if( $(this).hasClass('toggle-view') ){
+				nextModal(page, 1, true);
+			} else {
+				nextModal(page, 1, false);
+			}
+		});
+
+		$(".form-modals .previous-modal-button").click(function(event){
+			var page = $(this).closest('.mymodal').data("page");
+			nextModal(page, -1, false);
+		});
+
+		$('.form-modals .radio-input input').on('change', function() {
+			console.log($(this).get(0).value);
+			sendDataToIframe($(this).get(0).value);
+			$('.toggle-view').show();
+		});
+
+		function nextModal(page, tick, option){
+			var nextpage = page+tick;
+			var mymodals = $(".form-modals .mymodal");
+			mymodals.hide();
+			if( option ){
+				var myoption = $('.radio-input input:checked').val();
+				mymodals.not( ":not([data-page='"+(nextpage)+"'])" ).not( ":not([data-option='"+(myoption)+"'])" ).show();
+			}
+			else {
+				mymodals.not( ":not([data-page='"+(nextpage)+"'])" ).show();				
+			}
+		}
 	});
 </script>
 <?php get_footer(); ?>
